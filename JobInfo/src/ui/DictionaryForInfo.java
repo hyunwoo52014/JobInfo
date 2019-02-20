0c10ee3bbd8d18e72eeb6e1798d9058aa302c64f
Park Hyun Woo <merapasec@gmail.com>
2019-02-20 17:52:52 +0900

    2019-02-20 pm 17:52

---
+++ JobInfo/src/ui/DictionaryForInfo.java
@@ -0,0 +1,27 @@
+package ui;
+
+import java.io.File;
+
+public class DictionaryForInfo {
+	
+	private String filepath = null;
+	
+	public DictionaryForInfo(String filepath) {
+		this.filepath = filepath;
+	}
+	
+	public void setfilepath(String filepath) {
+		this.filepath = filepath;
+	}
+	
+	public String getCodeName(int index) {
+		
+		return null;
+	}
+	
+	public int getCodeNum(String name) {
+		
+		return 0;
+	}
+	
+}
