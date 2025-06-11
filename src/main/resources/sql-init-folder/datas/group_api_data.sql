df7f5ab57cf3504d3ecd51262cd7b4b10de96dc4
박현우 <hyunwoo52014@gmail.com>
2025-06-11 09:54:42 +0900

    추적 관리

--- src/main/resources/sql-init-folder/datas/group_api_data.sql
+++
@@ -1,3 +0,0 @@
-MERGE INTO ApiTable(orderValue, apiKey, headerValue) VALUES
-(1, 'erdO5SD8H2ecM7T3gIFRn4DEeSQmipLRvN3pAVPD4xt3yqC', 'application/xml'),
-(2, 'erdO5SD8H2ecM7T3gIFRn4DEeSQmipLRvN3pAVPD4xt3yqC', 'application/json');
